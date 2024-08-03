400792ed83a0f2899fb18dd567c3892ef5fca8bd
Szymon Witt <szymonwitt2707@gmail.com>
2024-08-03 11:59:24 +0200

    feat: add generation for wall-filled matrix

--- main.go
+++ main.go
@@ -6,6 +6,23 @@ const ROWS int32 = 12
 const COLUMNS int32 = 12
 const CELL_SIZE int32 = 50
 
+const WALL_CHAR byte = 'x'
+
+func generateWallFilledMatrix() *[][]byte {
+	matrix := make([][]byte, ROWS)
+	for i := range matrix {
+		matrix[i] = make([]byte, COLUMNS)
+	}
+
+	for r := range ROWS {
+		for c := range COLUMNS {
+			matrix[r][c] = WALL_CHAR
+		}
+	}
+
+	return &matrix
+}
+
 func main() {
 	const windowWidth int32 = COLUMNS * CELL_SIZE
 	const windowHeight int32 = ROWS * CELL_SIZE
@@ -15,10 +32,25 @@ func main() {
 
 	rl.SetTargetFPS(60)
 
+	matrix := generateWallFilledMatrix()
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
+
+		for r := range ROWS {
+			for c := range COLUMNS {
+				cellColor := rl.White
+				cell := (*matrix)[r][c]
+				switch cell {
+				case WALL_CHAR:
+					cellColor = rl.DarkBlue
+				}
+
+				rl.DrawRectangle(c*CELL_SIZE, r*CELL_SIZE, CELL_SIZE, CELL_SIZE, cellColor)
+			}
+		}
 
 		rl.EndDrawing()
 	}
