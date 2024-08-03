548fefc72f6d8f22c73f365a1c035f17af47c959
Szymon Witt <szymonwitt2707@gmail.com>
2024-08-03 14:09:37 +0200

    feat: add min heap implementation

---
+++ heap.go
@@ -0,0 +1,83 @@
+package main
+
+type HeapNode interface {
+	GetCost() float64
+}
+
+type MinHeap []HeapNode
+
+func (heap MinHeap) swapByIndex(i int, j int) {
+	heap[i], heap[j] = heap[j], heap[i]
+}
+
+func (heap MinHeap) leftChildIndex(i int) int {
+	return 2*i + 1
+}
+
+func (heap MinHeap) rightChildIndex(i int) int {
+	return 2*i + 2
+}
+
+func (heap MinHeap) parentIndex(i int) int {
+	return int((i - 1) / 2)
+}
+
+func (heap MinHeap) Len() int {
+	return len(heap)
+}
+
+func (heap *MinHeap) heapifyUp(i int) {
+	parentIdx := heap.parentIndex(i)
+	for (*heap)[parentIdx].GetCost() > (*heap)[i].GetCost() {
+		heap.swapByIndex(parentIdx, i)
+	}
+}
+
+func (heap *MinHeap) heapifyDown(i int) {
+	leftChildIdx := heap.leftChildIndex(i)
+	rightChildIdx := heap.rightChildIndex(i)
+
+	smallerChildIdx := i
+
+	if leftChildIdx <= heap.Len()-1 && (*heap)[smallerChildIdx].GetCost() > (*heap)[leftChildIdx].GetCost() {
+		smallerChildIdx = leftChildIdx
+	}
+
+	if rightChildIdx <= heap.Len()-1 && (*heap)[smallerChildIdx].GetCost() > (*heap)[rightChildIdx].GetCost() {
+		smallerChildIdx = rightChildIdx
+	}
+
+	if smallerChildIdx != i {
+		heap.swapByIndex(smallerChildIdx, i)
+		heap.heapifyDown(smallerChildIdx)
+	}
+}
+
+func (heap *MinHeap) Push(node HeapNode) {
+	*heap = append(*heap, node)
+
+	newValueIdx := heap.Len() - 1
+	parent := (*heap)[heap.parentIndex(newValueIdx)]
+	if parent.GetCost() > node.GetCost() {
+		heap.heapifyUp(newValueIdx)
+	}
+}
+
+func (heap *MinHeap) Pop() HeapNode {
+	if heap.Len() == 0 {
+		return nil
+	}
+
+	minNode := (*heap)[0]
+
+	// Swap the current root with the last value,
+	// discard the last value in the heap,
+	// and heapify down to retaing the heap property.
+
+	(*heap)[0] = (*heap)[heap.Len()-1]
+	*heap = (*heap)[0 : heap.Len()-1]
+
+	heap.heapifyDown(0)
+
+	return minNode
+}
---
+++ heap_test.go
@@ -0,0 +1,171 @@
+package main
+
+import "testing"
+
+type TestNode struct {
+	cost float64
+}
+
+func (testNode TestNode) GetCost() float64 {
+	return testNode.cost
+}
+
+func TestPushToEmptyHeap(t *testing.T) {
+	minHeap := &MinHeap{}
+	minHeap.Push(TestNode{7})
+
+	if minHeap.Len() != 1 {
+		t.Fatalf("Unexpected length of the heap. Expected 1 but got %d", minHeap.Len())
+	}
+
+	if (*minHeap)[0].GetCost() != 7 {
+		t.Fatalf("Unexpected root of the heap. Expected 7 but got %d", (*minHeap)[0])
+	}
+}
+
+func TestPushTwoElements(t *testing.T) {
+	minHeap := &MinHeap{}
+	minHeap.Push(TestNode{7})
+	minHeap.Push(TestNode{5})
+
+	if minHeap.Len() != 2 {
+		t.Fatalf("Unexpected length of the heap. Expected 2 but got %d", minHeap.Len())
+	}
+
+	if (*minHeap)[0].GetCost() != 5 {
+		t.Fatalf("Unexpected root of the heap. Expected 5 but got %d", (*minHeap)[0])
+	}
+
+	expectedList := []int{5, 7}
+	for idx := range expectedList {
+		if int((*minHeap)[idx].GetCost()) != expectedList[idx] {
+			t.Fatalf("Unexpected heap value at index %d. Expected %d but got %d", idx, expectedList[idx], (*minHeap)[idx])
+		}
+	}
+}
+
+func TestPushThreeElements(t *testing.T) {
+	minHeap := &MinHeap{}
+	minHeap.Push(TestNode{7})
+	minHeap.Push(TestNode{5})
+	minHeap.Push(TestNode{9})
+
+	if minHeap.Len() != 3 {
+		t.Fatalf("Unexpected length of the heap. Expected 3 but got %d", minHeap.Len())
+	}
+
+	if (*minHeap)[0].GetCost() != 5 {
+		t.Fatalf("Unexpected root of the heap. Expected 5 but got %d", (*minHeap)[0])
+	}
+
+	expectedList := []int{5, 7, 9}
+	for idx := range expectedList {
+		if int((*minHeap)[idx].GetCost()) != expectedList[idx] {
+			t.Fatalf("Unexpected heap value at index %d. Expected %d but got %d", idx, expectedList[idx], (*minHeap)[idx])
+		}
+	}
+}
+
+func TestPopHeapOneElement(t *testing.T) {
+	minHeap := &MinHeap{}
+	minHeap.Push(TestNode{7})
+
+	if minHeap.Len() != 1 {
+		t.Fatalf("Unexpected length of the heap. Expected 1 but got %d", minHeap.Len())
+	}
+
+	if (*minHeap)[0].GetCost() != 7 {
+		t.Fatalf("Unexpected root of the heap. Expected 7 but got %d", (*minHeap)[0])
+	}
+
+	expectedList := []int{7}
+	for idx := range expectedList {
+		if int((*minHeap)[idx].GetCost()) != expectedList[idx] {
+			t.Fatalf("Unexpected heap value at index %d. Expected %d but got %d", idx, expectedList[idx], (*minHeap)[idx])
+		}
+	}
+
+	popedNode := minHeap.Pop()
+	if popedNode.GetCost() != 7 {
+		t.Fatalf("Unexpected poped heap value. Expected 7 but got %f", popedNode.GetCost())
+	}
+
+	if minHeap.Len() != 0 {
+		t.Fatalf("Unexpected length of the heap. Expected 0 but got %d", minHeap.Len())
+	}
+}
+
+func TestPopHeapTwoElements(t *testing.T) {
+	minHeap := &MinHeap{}
+	minHeap.Push(TestNode{7})
+	minHeap.Push(TestNode{4})
+
+	if minHeap.Len() != 2 {
+		t.Fatalf("Unexpected length of the heap. Expected 2 but got %d", minHeap.Len())
+	}
+
+	if (*minHeap)[0].GetCost() != 4 {
+		t.Fatalf("Unexpected root of the heap. Expected 4 but got %d", (*minHeap)[0])
+	}
+
+	expectedList := []int{4, 7}
+	for idx := range expectedList {
+		if int((*minHeap)[idx].GetCost()) != expectedList[idx] {
+			t.Fatalf("Unexpected heap value at index %d. Expected %d but got %d", idx, expectedList[idx], (*minHeap)[idx])
+		}
+	}
+
+	popedNode := minHeap.Pop()
+	if popedNode.GetCost() != 4 {
+		t.Fatalf("Unexpected poped heap value. Expected 4 but got %f", popedNode.GetCost())
+	}
+
+	if minHeap.Len() != 1 {
+		t.Fatalf("Unexpected length of the heap. Expected 1 but got %d", minHeap.Len())
+	}
+
+	expectedList = []int{7}
+	for idx := range expectedList {
+		if int((*minHeap)[idx].GetCost()) != expectedList[idx] {
+			t.Fatalf("Unexpected heap value at index %d. Expected %d but got %d", idx, expectedList[idx], (*minHeap)[idx])
+		}
+	}
+}
+
+func TestPopHeapThreeElements(t *testing.T) {
+	minHeap := &MinHeap{}
+	minHeap.Push(TestNode{7})
+	minHeap.Push(TestNode{4})
+	minHeap.Push(TestNode{8})
+
+	if minHeap.Len() != 3 {
+		t.Fatalf("Unexpected length of the heap. Expected 3 but got %d", minHeap.Len())
+	}
+
+	if (*minHeap)[0].GetCost() != 4 {
+		t.Fatalf("Unexpected root of the heap. Expected 4 but got %d", (*minHeap)[0])
+	}
+
+	expectedList := []int{4, 7, 8}
+	for idx := range expectedList {
+		if int((*minHeap)[idx].GetCost()) != expectedList[idx] {
+			t.Fatalf("Unexpected heap value at index %d. Expected %d but got %d", idx, expectedList[idx], (*minHeap)[idx])
+		}
+	}
+
+	popedNode := minHeap.Pop()
+	if popedNode.GetCost() != 4 {
+		t.Fatalf("Unexpected poped heap value. Expected 4 but got %f", popedNode.GetCost())
+	}
+
+	if minHeap.Len() != 2 {
+		t.Fatalf("Unexpected length of the heap. Expected 1 but got %d", minHeap.Len())
+	}
+
+	expectedList = []int{7, 8}
+	for idx := range expectedList {
+		if int((*minHeap)[idx].GetCost()) != expectedList[idx] {
+			t.Fatalf("Unexpected heap value at index %d. Expected %d but got %d", idx, expectedList[idx], (*minHeap)[idx])
+		}
+	}
+}
