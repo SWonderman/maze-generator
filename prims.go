eba5db8d971e28b3e6e3450741379798f205d689
Szymon Witt <szymonwitt2707@gmail.com>
2024-08-04 16:38:52 +0200

    feat: add prims maze generation algorithm

--- prims.go
+++ prims.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"math/rand"
+)
+
 type Edge struct {
 	From   *GridNode
 	To     *GridNode
@@ -11,7 +15,7 @@ type GridNode struct {
 	Column int
 }
 
-func (node *GridNode) GetNeighbours(matrix *[][]byte, wallChar byte) []*Edge {
+func (node *GridNode) GetWallNeighbours(matrix *[][]byte, wallChar byte) []*Edge {
 	var edges []*Edge
 
 	row := node.Row
@@ -42,6 +46,37 @@ func (node *GridNode) GetNeighbours(matrix *[][]byte, wallChar byte) []*Edge {
 	return edges
 }
 
+func (node *GridNode) GetNeighbours(matrix *[][]byte, wallChar byte) []*Edge {
+	var edges []*Edge
+
+	row := node.Row
+	column := node.Column
+
+	orthogonalDirectionWeight := 1.0
+
+	// TOP
+	if row-1 >= 0 {
+		edges = append(edges, &Edge{node, &GridNode{Row: row - 1, Column: column}, orthogonalDirectionWeight})
+	}
+
+	// DOWN
+	if row+1 <= len(*matrix)-1 {
+		edges = append(edges, &Edge{node, &GridNode{Row: row + 1, Column: column}, orthogonalDirectionWeight})
+	}
+
+	// RIGHT
+	if column+1 <= len((*matrix)[0])-1 {
+		edges = append(edges, &Edge{node, &GridNode{Row: row, Column: column + 1}, orthogonalDirectionWeight})
+	}
+
+	// LEFT
+	if column-1 >= 0 {
+		edges = append(edges, &Edge{node, &GridNode{Row: row, Column: column - 1}, orthogonalDirectionWeight})
+	}
+
+	return edges
+}
+
 type PrimsNode struct {
 	CurrentNode *GridNode
 	LastEdge    *Edge
@@ -63,6 +98,7 @@ func containsNode(haystack []*GridNode, needle *GridNode) bool {
 	return contains
 }
 
+// 'Default' implementation of Prim's
 func RunPrims(matrix *[][]byte, start *GridNode, obstacleChar byte) []*Edge {
 	visited := []*GridNode{}
 
@@ -98,4 +134,52 @@ func RunPrims(matrix *[][]byte, start *GridNode, obstacleChar byte) []*Edge {
 	}
 
 	return minimumSpanningTree
+}
+
+func RunMazeGeneratingPrims(matrix *[][]byte, start *GridNode, obstacleChar byte) {
+	visited := []*GridNode{}
+	// NOTE: In this version of the algorithm, the pqueue is really just a list/array
+	// from which values will be drawn at 'random'
+	frontiers := []*PrimsNode{}
+
+	startPrimsNode := &PrimsNode{start, nil, nil, 0}
+	(*matrix)[startPrimsNode.CurrentNode.Row][startPrimsNode.CurrentNode.Column] = '+'
+
+	frontiers = append(frontiers, startPrimsNode)
+
+	for _, edge := range start.GetNeighbours(matrix, obstacleChar) {
+		primsNode := &PrimsNode{edge.To, edge, startPrimsNode, edge.Weight}
+		frontiers = append(frontiers, primsNode)
+	}
+
+	for len(frontiers) > 0 {
+		randomFrontierIdx := rand.Intn(len(frontiers))
+		frontier := frontiers[randomFrontierIdx]
+		frontiers = append(frontiers[:randomFrontierIdx], frontiers[randomFrontierIdx+1:]...)
+
+		passagesAroundTheFrontier := 0
+		for _, edge := range frontier.CurrentNode.GetNeighbours(matrix, obstacleChar) {
+			if (*matrix)[edge.To.Row][edge.To.Column] == '+' {
+				passagesAroundTheFrontier += 1
+			}
+		}
+
+		if containsNode(visited, frontier.CurrentNode) {
+			continue
+		}
+
+		if passagesAroundTheFrontier == 1 {
+			visited = append(visited, frontier.CurrentNode)
+
+			(*matrix)[frontier.CurrentNode.Row][frontier.CurrentNode.Column] = '+'
+
+			for _, edge := range frontier.CurrentNode.GetNeighbours(matrix, obstacleChar) {
+				if !containsNode(visited, edge.To) {
+					primsNode := &PrimsNode{edge.To, edge, frontier, edge.Weight}
+					frontiers = append(frontiers, primsNode)
+				}
+			}
+		}
+
+	}
 }
